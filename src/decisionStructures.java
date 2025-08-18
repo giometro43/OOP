3a14dab3646e57546f7e23328f477e0302fa134c
giometro43 <giocdbz@gmail.com>
2025-08-17 21:05:00 -0700

    Update to input validation and logic for grade output

--- src/decisionStructures.java
+++ src/decisionStructures.java
@@ -1,9 +1,9 @@
 /*
 Ch.3 Expectations:
-- Read numeric grade [0..100] via Scanner
-- Use if/else-if/else with relational + logical ops
-- Validate input (out-of-range => error)
-- Print letter grade A/B/C/D/F
+- Read numeric grade [0..100] via Scanner (Y)
+- Use if/else-if/else with relational + logical ops (Y)
+- Validate input (out-of-range => error) (Y)
+- Print letter grade A/B/C/D/F (Y)
 */
 import java.util.Scanner;
 import java.lang.Throwable;
@@ -11,7 +11,7 @@ public class decisionStructures {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		
 		Scanner in = new Scanner(System.in);
 		
@@ -19,21 +19,25 @@ public class decisionStructures {
 		
 		System.out.println("Please input a grade 1-100.");
 		int grade = in.nextInt();
-		
-		if(grade > 100 || grade < 1 ) {
+		// input validation, needs to be an integer
+		if(grade > 100 || grade <= -1 ) {
+			//NOTE: change if loop to while since it only repeats once
+			/*Logic explanation: if grade is less than or equal
+			 *to -1 (Neg inf) OR greater than 100 (positive if)
+			 leaves range 0-100 */
 			System.out.println("Please input a grade 1-100.");
 			grade = in.nextInt();   
 		}
-		else if(grade <=100 || grade >=90){
+		else if(grade <=100 && grade >=90){  
 			System.out.print( "GRADE : A");
 		}
-		else if(grade <=89 || grade >=80){
+		else if(grade <=89 && grade >=80){
 			System.out.print( "GRADE : B");
 		}
-		else if(grade <=79 || grade >=70){
+		else if(grade <=79 && grade >=70){
 			System.out.print( "GRADE : C");
 		}
-		else if(grade <=69 || grade >=60){
+		else if(grade <=69 && grade >=60){
 			System.out.print( "GRADE : D");
 		}
 		else if(grade <= 59){
