0477f6223f3ef956d6b7dd87067e430501e41f9a
giometro43 <giocdbz@gmail.com>
2025-09-03 16:33:01 -0700

    Start P157

--- src/m1b/P157DateConverter.java
+++ src/m1b/P157DateConverter.java
@@ -1,14 +1,38 @@
 package m1b;
+import java.util.Scanner;
 /**@author Giovanni Castillo AIT502-11
-Module: 
-Assignment: 
-Program: 
+Module: Conditionals
+Assignment: M1B-21
+Program: 157 Date Converter
 */
 public class P157DateConverter {
-
+	/*Requirements: Read a current date from the keyboard as a string in the format MM/DD/YYYY (e.g. 2/4/2021, 12/2/2020)
+	 * If the date is wrong provide an error message. If the date is correct, convert it in the format: 
+	 * MONTH DD, YYYY (e.g. February 4, 2021; December 2, 2020). 
+	 * You may need to check if there year is a leap year or not. 
+	 * Do not use any predefined method for processing the date. Write the algorithm yourself
+	 */
 	public static void main(String[] args) {
-		System.out.println("PROGRAM NAME by Giovanni Castillo on DUE/DATE/2025");
-
+	
+		System.out.println("Date Converter by Giovanni Castillo on 08/31/2025");
+		Scanner in = new Scanner(System.in);
+		
+		
+		System.out.println("Enter a date in the format mm/dd/yyyy: ");
+		String dateRaw = in.nextLine();
+		
+		int dateMonthint = dateRaw.indexOf("/");
+		String dateMonth = dateRaw.substring(0 , dateMonthint);
+		System.out.println(dateMonth);
+		
+		int dateDayint = dateRaw.indexOf("/", dateMonthint + 1 );
+		String dateDay = dateRaw.substring(dateDayint, dateMonthint);
+		System.out.println(dateDay);
+		
+		int dateYearint = dateRaw.indexOf("/");
+		String dateYear = dateRaw.substring(5 , dateYearint);
+		System.out.println(dateYear);
+		
 	}
 
 }
