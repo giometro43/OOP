953a60b8cc221aef0b4ba2e88994d6dc7f4cfe1e
giometro43 <giocdbz@gmail.com>
2025-09-04 22:46:32 -0700

    Completed classes 211 and 212

--- src/m2a/P211GradesAnalysis.java
+++ src/m2a/P211GradesAnalysis.java
@@ -1,4 +1,5 @@
 package m2a;
+import java.util.Scanner;
 /**@author Giovanni Castillo AIT502-11
 Module: Loops
 Assignment: M2A-21
@@ -18,7 +19,62 @@ public class P211GradesAnalysis {
 
 	public static void main(String[] args) {
 		System.out.println("Grade Analysis by Giovanni Castillo on 09/07/2025");
+		/*starting by going into the run configurations and 
+		adding the file path to the arguments from the 
+		Run Config > Common > Standard Input and Output > Input file*/
 		
+		
+		// read numbers from the grades.txt file then print them in the console
+		//seperated by space
+		Scanner in = new Scanner(System.in);
+		System.out.print("The grades are: ");
+		
+		double grade = Double.parseDouble(in.next());//this reads the first Token
+		//initialize computations:
+		int numberOfStudents = 0;
+		double maximumGrade = Double.NEGATIVE_INFINITY; //smallest possible number
+		//guarantees that any grade will be larger than this number
+		double minimumGrade = Double.POSITIVE_INFINITY; //largest possible number
+		double sumOfGrades = 0;
+		double averageGrade = 0;
+		int studentsWithA = 0;
+		
+		while (grade >=0) {
+			//print the grade
+			System.out.print(grade + " ");
+			//process the grade
+			numberOfStudents++;//increment the number of students by 1
+			sumOfGrades += grade; // adds every grade to the sumgrades val
+			averageGrade = sumOfGrades/numberOfStudents; //gives average
+			
+			if(grade > maximumGrade) {
+				maximumGrade = grade;
+				/*this ensures that the maximum grade is updated to the current grade 
+				if it's higher. so that at the end of the loop, 
+				maximumGrade holds the highest grade*/
+				
+			}
+			if(grade < minimumGrade) {
+				minimumGrade = grade;
+				
+			}
+			if (grade >=90) {
+				studentsWithA++;
+			}
+			//read the next token
+			 grade = Double.parseDouble(in.next());
+			
+		}
+		System.out.println();
+		in.close();
+		//print the number of students
+		System.out.println("The number of students is: " + numberOfStudents);
+		System.out.println("the maxiumum grade is: "+maximumGrade);
+		System.out.println("the minimum grade is: "+minimumGrade);
+		System.out.println("Average grade is: "+averageGrade);
+		System.out.println("Students with an A score: "+ studentsWithA);
+
+
 
 	}
 
--- src/m2a/P212FriendsHeight.java
+++ src/m2a/P212FriendsHeight.java
@@ -1,4 +1,5 @@
 package m2a;
+import java.util.Scanner;
 /**@author Giovanni Castillo AIT502-11
 Module: Loops
 Assignment: M2A-21
@@ -10,10 +11,46 @@ public class P212FriendsHeight {
  * as positive real numbers. 
  * Compute the number of friends, the maximum height (in feet and inches),
  *  the minimum height (in feet and inches), and the average height of your friends.*/
-
 	public static void main(String[] args) {
-		System.out.println("Friends Height by Giovanni Castillo on 09/07/2025");
+		Scanner in = new Scanner(System.in);
 
+		System.out.println("Friends Height by Giovanni Castillo on 09/07/2025");
+		System.out.println("The heights are: ");
+
+		double height = Double.parseDouble(in.next());
+		//initialize computations:
+		int numberOfFriends=0;
+		double minimumHeight = Double.POSITIVE_INFINITY;
+		double maximumHeight = Double.NEGATIVE_INFINITY;
+		double sumOfHeight = 0;
+		double averageHeight = 0;
+		
+		
+			while(height>=0) {
+			System.out.print(height + " ");
+			numberOfFriends++;
+			sumOfHeight +=height;
+			averageHeight = sumOfHeight/numberOfFriends;
+			
+				if(height > maximumHeight) {
+					maximumHeight = height;
+					}
+					
+				if(height < minimumHeight) {
+					minimumHeight = height;
+					}
+			
+			height = Double.parseDouble(in.next());
+			}
+		
+		System.out.println();
+		in.close();
+		//print the final response for friend height
+		System.out.println("The number of friends is " + numberOfFriends);
+		System.out.println("the max height is: "+maximumHeight);
+		System.out.println("The min height is: "+minimumHeight);
+		System.out.println("the average height is: "+ averageHeight);
+		
 	}
 
 }
